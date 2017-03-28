efabbacaa9ba62b6c6cda5cc9db3fb50e30dc3a0
liuwei <i@liuwei.co>
2017-03-28 16:54:37 +0800

    ok

---
+++ General/Vendors/OpenSSL/OpenSSLUtil.h
@@ -0,0 +1,13 @@
+//
+//  OpenSSLUtil.h
+//  BaseSwiftProject
+//
+//  Created by 刘伟 on 3/28/17.
+//  Copyright © 2017 上海凌晋信息技术有限公司. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenSSLUtil : NSObject
+
+@end
---
+++ General/Vendors/OpenSSL/OpenSSLUtil.m
@@ -0,0 +1,13 @@
+//
+//  OpenSSLUtil.m
+//  BaseSwiftProject
+//
+//  Created by 刘伟 on 3/28/17.
+//  Copyright © 2017 上海凌晋信息技术有限公司. All rights reserved.
+//
+
+#import "OpenSSLUtil.h"
+
+@implementation OpenSSLUtil
+
+@end
